b8d6048cc5052a4a98fa3c2aa773e1a4ab3e6346
Aaron Brooks <aaron.brooks@embl.de>
2015-10-06 16:39:25 +0200

    updated gRNA design script - from genome 2 correct format

--- scripts/design_gRNA_func.R
+++ scripts/design_gRNA_func.R
@@ -2,33 +2,40 @@
 # Author: czhu
 ###############################################################################
 
-design_basic_gRNA = function(seqs) {
-    ## given a sequence, find the PAM site (with PAM.MOTIF) and retrieve the sequence as gRNA 
-    ## if the given sequence is not long enough (at the boundary), it ignores these PAM sites 
+design_basic_gRNA = function(chr, start, end, genome) {
+    ## genome is the genome sequence as DNAStringSet object
+
+    ## given a sequence, find the PAM site (with PAM.MOTIF) and retrieve the sequence as gRNA
+    ## if the given sequence is not long enough (at the boundary), it ignores these PAM sites
     require(Biostrings)
     GRNA.LENGTH=20
     ## NGG
     PAM.MOTIF ="GG"
+
+    # need to get this from chr, start, end
+    # do it for both strands
+    seqs =
+
     mysubseq = function(x,ir) {
-        ## given ir, IRanges 
+        ## given ir, IRanges
         ## x, DNAStringSet
-        ## retrieve sequences 
+        ## retrieve sequences
         if(length(ir)>0 & (sum((start(ir) -GRNA.LENGTH)>0)>0)) {
             sel = which((start(ir) - 1 - GRNA.LENGTH)>0)
-            
+
             rv = data.frame(
                 pStart =  start(ir)[sel] - 1 - GRNA.LENGTH,
                 pEnd   =  end(ir)[sel] - nchar(PAM.MOTIF) - 1, ## minus the N site
-                stringsAsFactors=FALSE)            
-            rv$seq = as.character(subseq(rep(x, length(sel)), 
+                stringsAsFactors=FALSE)
+            rv$seq = as.character(subseq(rep(x, length(sel)),
                     start=rv$pStart, end=rv$pEnd))
             rv
-                
+
         } else {
             rv = NULL
         }
         rv
-    } 
+    }
 #    if(is.null(names(seqs))) {
 #        names(seqs) = paste0("target",1:length(seqs))
 #    }
@@ -38,7 +45,7 @@ design_basic_gRNA = function(seqs) {
     pam_pos = lapply(pam_pos, function(x){
             rv = reduce(x)
             sel = width(rv)>nchar(PAM.MOTIF)
-            start(rv[sel]) = start(rv[sel]) + 1 
+            start(rv[sel]) = start(rv[sel]) + 1
             end(rv[sel]) = start(rv[sel]) + 1
             rv
         })
@@ -47,17 +54,22 @@ design_basic_gRNA = function(seqs) {
         }
     )
     names(gRNA.seq) = names(seqs)
+
+    # desired output
+    # seqnames	start	end	width	strand	seq	gc
+    # chr04	411862	411881	20	-	TCTACCAGCATTCAAGTGGC	0.5
+
     rv = data.frame(
         index=rep(1:length(seqs), sapply(gRNA.seq,nrow)),
         gSeq=unlist(sapply(gRNA.seq, "[","seq")),
         pStart=unlist(sapply(gRNA.seq, "[","pStart")),
         pEnd=unlist(sapply(gRNA.seq, "[","pEnd")),
-        stringsAsFactors=FALSE)    
+        stringsAsFactors=FALSE)
     rv
 }
 
 
-assess_bowtie2 = function(candi) {    
+assess_bowtie2 = function(candi) {
     ################################################################################
     ## method 1 use bowtie
     ## limitation, only consider cases with 1 MM
@@ -65,33 +77,33 @@ assess_bowtie2 = function(candi) {
     N.MISMATCH = 1
     BW.INDEX= "/g/steinmetz/czhu/genome/S288c/indexes/bowtie2/s288c_20110203"
     require(GenomicAlignments)
-    
+
     fafile = tempfile()
     alnfile = tempfile()
-    
+
     writeXStringSet(candi, filepath=fafile)
     cat("Writing fasta to", fafile, "\n")
-    cmd = paste("bowtie2 -x", BW.INDEX, 
-        "-f", fafile ,"--very-sensitive -N 1 --all -L 23 | samtools view -b - >", alnfile) 
+    cmd = paste("bowtie2 -x", BW.INDEX,
+        "-f", fafile ,"--very-sensitive -N 1 --all -L 23 | samtools view -b - >", alnfile)
     cat("Running",cmd,"\n")
     system(cmd)
     myparam = ScanBamParam(tag=c("XM", "MD"),what=c("qname"))
     ga = readGAlignments(alnfile, param=myparam)
-    
+
     unlink(c(fafile,alnfile))
     ## only reads with 1 mismatch, no gaps
     ga= ga[cigar(ga)==paste0(qwidth(ga),"M")]
     ga= ga[values(ga)$XM<2]
-    
+
     if(length(ga)==0){
         return(NULL)
     } else {
         spl = strsplit(values(ga)$MD, "\\D")
-        
+
         mmpos = rep(0,length(ga))
         hasMM = sapply(spl, length)>1
         mmpos[hasMM] = as.integer(sapply(spl[hasMM],"[",1)) + 1
-        
+
         da = data.frame(
             subject = values(ga)$qname,
             chr= seqnames(ga),
@@ -104,17 +116,17 @@ assess_bowtie2 = function(candi) {
             stringsAsFactors=FALSE)
         return(da)
     }
-    
+
 }
 
 ## ##############################################################################
-## method 2 use R's pattern recogniction. This is much slower but should be more accurate 
+## method 2 use R's pattern recogniction. This is much slower but should be more accurate
 ## this is not implemented
 #lapply(names(genome), function(chr){
 #        plus_hits = matchPDict(candi,genome[[chr]],max.mismatch=N.MISMATCH)
 #    })
 offtarget_analysis = function(candi){
-    
+
     if(class(candi) != "DNAStringSet") {
         candi = DNAStringSet(candi)
     }
@@ -136,15 +148,15 @@ offtarget_analysis = function(candi){
                     sel = which(x$nMM ==1)
                     rv$offtargets = paste(paste(x$chr,x$start,x$end,x$strand,sep="_")[sel], collapse=",")
                     rv$mutPos = paste(x$MMpos[sel], collapse=",")
-                } 
+                }
                 rv
             })
         finalrp=do.call(rbind,finalrp)
-        return(finalrp)        
+        return(finalrp)
     }
 }
 ## for backward compatibility
-offsite_analysis = offtarget_analysis 
+offsite_analysis = offtarget_analysis
 
 ## extract sequence from a given genome
 getseq = function(dat,genome){
@@ -152,22 +164,20 @@ getseq = function(dat,genome){
     ## genome is the genome sequence as DNAStringSet object
     require(Biostrings)
     rv = DNAStringSet(lapply(1:nrow(dat), function(i){
-                subseq(genome[[dat$chr[i]]], start=dat$start[i], end=dat$end[i]) 
+                subseq(genome[[levels(dat$chr)[dat$chr[i]]]],
+                  start=as.numeric(levels(dat$start)[dat$start[i]]),
+                  end=as.numeric(levels(dat$end)[dat$end[i]]))
             }))
     isMinus = which(dat$strand == "-")
     if(length(isMinus)>0) {
-        rv[isMinus] = reverseComplement(rv[isMinus])       
+        rv[isMinus] = reverseComplement(rv[isMinus])
     }
     rv
 }
 
-
 gcContent <-function(x) {
-    if(is.character(x)) 
+    if(is.character(x))
         x = DNAStringSet(x)
     alf <- alphabetFrequency(x, as.prob=TRUE)
     rowSums(alf[,c("G", "C"),drop=FALSE])
 }
-
-
-
